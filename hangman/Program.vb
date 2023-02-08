2204dcef49e0be15fce1777d63fe8d225cffd9a8
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-02-08 14:47:11 -0500

    init vb project

---
+++ hangman/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim FirstName As String
+		FirstName = "Gavin"
+		Dim LastName As String
+		LastName = "Gilbert"
+		Console.WriteLine("Hello, " & FirstName & " " & LastName & ".")
+    End Sub
+End Module
