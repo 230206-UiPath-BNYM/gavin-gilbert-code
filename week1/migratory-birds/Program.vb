4477d5e47f017e39a3a5549723d873876ed3afb4
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-02-08 20:52:46 -0500

    takes input from user

--- week1/migratory-birds/Program.vb
+++ week1/migratory-birds/Program.vb
@@ -4,10 +4,26 @@ Module Program
 
     Sub Main(args As String())
 
-		Dim arr = New Integer() { 1, 1, 2, 2, 3 }
+		Dim input As String
+		Dim numSightings As Integer
+		Dim temp As Integer = 0
+		Dim lst As New List(of Integer)
+
+		Console.WriteLine("How many sightings would you like to enter: ")
+		input = Console.ReadLine()
+		Integer.TryParse(input, numSightings)
+
+
+		For i As Integer = 1 To numSightings
+			Console.WriteLine("Enter Sighting {0}: ", i)
+			input = Console.ReadLine()
+			Integer.TryParse(input, temp)
+			lst.add(temp)
+		Next
+		
 		Dim sightings As New Dictionary(Of Integer, Integer)
 
-		For Each num As Integer In arr
+		For Each num As Integer In lst
 			If sightings.ContainsKey(num) Then
 				sightings(num) = sightings(num) + 1
 			Else
@@ -16,9 +32,19 @@ Module Program
 
 		Next
 
+		Dim mostSpotted As Integer
+		Dim maxSpots As Integer = 0
+
 		For Each sighting As KeyValuePair(Of Integer, Integer) In sightings
-			Console.WriteLine("{0}: {1}", sighting.Key, sighting.Value)
+			If sighting.Value > maxSpots Then
+				maxSpots = sighting.Value
+				mostSpotted = sighting.Key
+			End If
 		Next
+
+		Console.WriteLine()
+		Console.WriteLine("Most Sighted: {0}", mostSpotted)
+		Console.WriteLine("# of Sightings: {0}", maxSpots)
 
     End Sub
 
